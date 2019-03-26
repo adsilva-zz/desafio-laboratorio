59dbe7c2776e47825ab4e48a831323ccc8100be2
Amanda da Silva Ito <adsilva32@gmail.com>
2019-03-26 17:23:18 -0300

    Criacao do endPoint cadastrar laboratorio

--- src/main/java/com/desafio/laboratorio/controle/LaboratorioControle.java
+++ src/main/java/com/desafio/laboratorio/controle/LaboratorioControle.java
@@ -1,10 +1,30 @@
 package com.desafio.laboratorio.controle;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.desafio.laboratorio.dto.LaboratorioDTO;
+import com.desafio.laboratorio.entidade.Laboratorio;
+import com.desafio.laboratorio.servico.LaboratorioServico;
 
 @RestController
 @RequestMapping("api/v1/laboratorios")
 public class LaboratorioControle {
 
+	@Autowired
+	private LaboratorioServico laboratorioServico;
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Laboratorio> cadastrarLaboratorio(@Valid @RequestBody LaboratorioDTO laboratorioDTO) {
+
+		return new ResponseEntity<Laboratorio>(laboratorioServico.cadastrarLaboratorio(laboratorioDTO),
+				HttpStatus.CREATED);
+	}
 }
