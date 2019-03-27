3d6d52651794b162715484df4b0ee663c62a73b6
Amanda da Silva Ito <adsilva32@gmail.com>
2019-03-27 14:05:07 -0300

    Ajuste de endpoint de atualização com DTO de Atualização

--- src/main/java/com/desafio/laboratorio/controle/LaboratorioControle.java
+++ src/main/java/com/desafio/laboratorio/controle/LaboratorioControle.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.desafio.laboratorio.dto.AtualizarLabDTO;
 import com.desafio.laboratorio.dto.LaboratorioDTO;
 import com.desafio.laboratorio.entidade.Laboratorio;
 import com.desafio.laboratorio.servico.LaboratorioServico;
@@ -51,7 +52,7 @@ public class LaboratorioControle {
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Laboratorio> atualizarLaboratorio(@PathVariable(value = "id") Long id,
-			@Valid @RequestBody LaboratorioDTO laboratorioDTO) {
+			@Valid @RequestBody AtualizarLabDTO laboratorioDTO) {
 		Laboratorio lab = laboratorioServico.atualizarLaboratorio(laboratorioDTO, id);
 		if (ObjectUtils.isEmpty(lab)) {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
